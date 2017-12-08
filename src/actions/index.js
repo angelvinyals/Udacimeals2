f9acdc4750ed4a10e205aeaebbc0d7cc20ebea68
angelvinyals <angel.vinyals@gmail.com>
2017-12-08 16:43:17 +0100

    create Actions
    created ADD-RECIPE, and REMOVE_FROM_CALENDAR actions

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+// Create An Action Creator
+
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
+ 
+export function addRecipe ({day,recipe, meal}) {
+	return{
+  		type: ADD_RECIPE,
+  		recipe,
+  		day,
+  		meal,
+	}
+}
+
+
+export function removeFromCalendar ({day,meal}){
+	return {
+		type: REMOVE_FROM_CALENDAR,
+		day,
+		meal,
+	}
+	
+}
