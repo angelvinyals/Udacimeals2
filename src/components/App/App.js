f6786e6160a1de26908fb5724615671db2a291a4
angelvinyals <angel.vinyals@gmail.com>
2017-12-15 15:34:35 +0100

    mapStateToProps
    add function and import connect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 
 	render() {
 		
-				//console.log(this.props.store.dispatch(receiveComment('Redux is great!')));
+		console.log(this.props);
 		return (
 		  <div className="App">
 		  	hola
 		  </div>
 		);
-		}
+	}
 }
 
-export default App;
+function mapStateToProps(state){
+	console.log(`calendar: ${state}`)
+	let days=[]
+	for(let day in state){
+		days.push(day) 
+	}
+	console.log(`days: ${days}`)
+	return {
+		calendar: days.map((day) =>({
+			day,
+			meals: Object.keys(state[day]).reduce((meals, meal) =>{
+						meals[meal] = state[day][meal]
+						? state[day][meal]
+						: null
+						return meals
+					},{})
+		}))
+	}
+}
+
+
+	
+}
+
+export default connect(mapStateToProps)(App)
 
 
