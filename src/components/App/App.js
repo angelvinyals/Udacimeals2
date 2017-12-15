ef078cdaaa9876f7602d9455b79d64fd7e8d0357
angelvinyals <angel.vinyals@gmail.com>
2017-12-15 13:02:05 +0100

    remove stuff for implementig react-redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,66 +1,14 @@
 import React, { Component } from 'react';
-import {addRecipe} from '../../actions'
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-	
-
-	state ={
-		calendar: null,
-		inputValue: '',
-		value:''
-	}
-
-	componentDidMount(){
-		const {store} = this.props
-		store.subscribe(() =>{
-			this.setState(() => ({
-				calendar: store.getState()
-			}))
-		})
-		
-	}
-
-	
-
-	handleChange = (event) =>{	
-		this.setState({inputValue:  event.target.value})
-	}
-
-	submitFood = (e) =>{
-		const {store} = this.props
-		console.log(`event: ${e}`)
-		store.dispatch(addRecipe({
-			day: 'monday',
-			meal: 'breakfast',
-			recipe:{
-				label: this.state.inputValue
-			}
-		}))
-		this.setState({inputValue: ''})
-		
-		e.preventDefault()
-		
-	}
 
 	render() {
 		
 				//console.log(this.props.store.dispatch(receiveComment('Redux is great!')));
 		return (
 		  <div className="App">
-		  	<form>
-			  <label>
-			    recipe for monday's breakfast
-			    <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
-			  </label>
-			  <input type="submit"  onClick={this.submitFood} />
-			</form>
-		    
-		    <pre>
-		    	Esmorzar del dilluns: {this.state.calendar && this.state.calendar.monday.breakfast}
-		    </pre>
+		  	hola
 		  </div>
 		);
 		}
@@ -69,16 +17,3 @@ class App extends Component {
 export default App;
 
 
-/*
-
-
-<input
-		    	type='text'
-		    	ref={(input) => this.input = input}
-		    />
-		    <button
-		    	onClick={this.submitFood}>
-		    submit
-		    </button>
-
-		    */
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import {createStore} from 'redux';
-import { Provider } from 'react-redux'
+
 import reducer from './reducers'
 
 const store = createStore(
